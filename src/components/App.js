b02da539fc15ca0b5265eb54dcf187f8755ffe9f
dmytro vitenko <cubagw84@gmail.com>
2019-10-24 09:24:04 +0300

    2 hw starting

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Reader from './Reader/Reader';
+// import Stats from './Stats/Stats';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
+// import FriendList from './FriendList/FriendList';
+
+// import user from './Profile/user.json';
+// import statsConfig from './Stats/statsConfig.json';
+// import transactions from './TransactionHistory/transactions.json';
+// import friends from './FriendList/friends.json';
+
+const App = () => <Reader items={publications} />;
+export default App;

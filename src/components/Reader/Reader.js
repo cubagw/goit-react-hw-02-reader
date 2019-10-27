de5da979ddf7c08c94ac3a813ee81eb91f868a9c
dmytro vitenko <cubagw84@gmail.com>
2019-10-27 22:44:04 +0200

    reader prefinish

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import T from 'prop-types';
+import styles from './Reader.module.css';
 
 import Controls from '../Controls/Controls';
 import Counter from '../Counter/Counter';
@@ -7,36 +8,49 @@ import Publication from '../Publication/Publication';
 
 export default class Reader extends Component {
   static propTypes = {
-    items: T.arrayOf(
-      T.shape({
-        id: T.string.isRequired,
-        text: T.string.isRequired,
-        title: T.string.isRequired,
-      }),
-    ).isRequired,
+    items: T.arrayOf().isRequired,
   };
 
   state = {
-    currenPage: 0,
+    currentPage: 0,
   };
 
   nextPage = () => {
-    this.setState(prevState => ({
-      currenPage: prevState.currentArticle + 1,
-    }));
+    this.state.currentPage + 1 < this.props.items.length &&
+      this.setState(prevState => ({
+        currentPage: prevState.currentPage + 1,
+      }));
   };
 
   prevPage = () => {
-    this.setState(prevState => ({
-      currenPage: prevState.currenPage - 1,
-    }));
+    this.state.currentPage + 1 > 1 &&
+      this.setState(prevState => ({
+        currentPage: prevState.currentPage - 1,
+      }));
   };
 
   render() {
+    const { currentPage } = this.state;
+    const { items } = this.props;
+
     return (
-      <div>
-        {/* <h2>Quiz</h2>
-        <p>{this.props.questions[this.state.questionIdx]}</p> */}
+      <div className={styles.reader}>
+        {/* <!-- Разметка компонента <Controls> --> */}
+        <Controls
+          onNextPage={this.nextPage}
+          onPrevPage={this.prevPage}
+          nextPageDisabled={currentPage >= items.length - 1}
+          prevPageDisabled={currentPage <= 0}
+        />
+
+        {/* <!-- Разметка компонента <Counter> --> */}
+        <Counter
+          totalNumberOfPages={items.length}
+          numberOfCurrentPage={currentPage + 1}
+        />
+
+        {/* <!-- Разметка компонента <Publication> --> */}
+        <Publication item={items[currentPage]} />
       </div>
     );
   }
